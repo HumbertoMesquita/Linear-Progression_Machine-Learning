62b3be21aa6a2219eabaa846f5a82409ebee06ad
HumbertoMesquita <humbertoalvesti@gmail.com>
2025-02-04 10:42:26 -0300

    Training

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ model = LinearRegression()
 x = df[["diametro"]]
 y = df[["preco"]]
 
-model.fit(x,y)
+model.fit(x, y)
 
 st.title("Prevendo o valor de uma pizza")
+st.divider()
+
+diametro = st.number_input("Digite o tamanho do diâmetro da pizza:")
+
+if diametro:
+    preco_previsto = model.predict([[diametro]])[0][0]
+    st.write(f"O valor da pizza com diâmetro de {diametro:.2f} cm é de R${preco_previsto:.2f}")
